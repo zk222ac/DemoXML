2e661c1b197c38f38d9495e8183360c233638e30
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-03-04 22:01:32 +0100

    Add project files.

---
+++ DemoXML/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+
+namespace DemoXML
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           // If you enable this code 
+           XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+           //XmlReader xmlReader = XmlReader.Create("AmbAir18022016.xml");
+           while (xmlReader.Read())
+           {
+               // .........ON and OFF this lines of code.....................................................
+
+               if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+               {
+                   if (xmlReader.HasAttributes)
+                   {
+                       Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));
+
+                   }
+               }
+
+
+               // .........ON and OFF this lines of code.....................................................
+               //if ((xmlReader.NodeType == XmlNodeType.Element))
+               //{
+               //    if (xmlReader.Name == "Maalested")
+               //        Console.WriteLine(xmlReader.ReadInnerXml());
+               //    if (xmlReader.Name == "StofNavn")
+               //       Console.WriteLine(xmlReader.ReadInnerXml());
+               //    if (xmlReader.Name == "RaaResultat")
+               //        Console.WriteLine(xmlReader.ReadInnerXml());
+               //}
+           }
+
+           Console.ReadKey();
+       }
+   }
+}
+
---
+++ dbo.Udeluft.data.sql
@@ -0,0 +1,27 @@
+﻿INSERT INTO Udeluft VALUES( '<Data>
+
+<MaaleStedId>111</MaaleStedId>
+
+<Maalested>Aalborg Tag ny</Maalested>
+
+<DatoMaerke>2015-02-19T00:00:00+01:00</DatoMaerke>
+
+<StofId>147</StofId>
+
+<StofNavn>Ozon</StofNavn>
+
+<EnhedId>2</EnhedId>
+
+<Enhed>ppb</Enhed>
+
+<RaaResultat>26.77</RaaResultat>
+
+<UdstyrId>2</UdstyrId>
+
+<Udstyr>API Monitor O3</Udstyr>
+
+<Easting_32>556479</Easting_32>
+
+<Northing_32>6322958</Northing_32>
+
+</Data>')
